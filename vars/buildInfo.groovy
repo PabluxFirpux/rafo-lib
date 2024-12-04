88db999074bc3e20c9d99b2ce66f3a6182e457bf
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-04 18:16:32 +0100

    c

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,11 +1,7 @@
 import global.*
 
 def call() {
-    Info currentInfo = new Info();
-    def job_name = currentInfo.job_name;
-    echo "${job_name}"
-    echo "${env}"
-    echo "${currentInfo.env}"
     echo "${JOB_NAME}"
-    echo "${currentInfo}"
+    echo "${WORKSPACE}"
+    helloWorld("pepe");
 }
