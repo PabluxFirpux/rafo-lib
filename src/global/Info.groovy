810a2650346183e4c3a6f7f3046ed9f1f91d58f5
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-04 17:58:41 +0100

    c

--- src/global/Info.groovy
+++ src/global/Info.groovy
@@ -1,9 +1,12 @@
 package global;
+import System;
 
 class Info {
+    def env;
     String job_name;
 
     Info() {
+        this.env = System.getenv()
         this.job_name = env.JOB_NAME;
     }
 }
