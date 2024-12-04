dcc29fe31e87530c75d8a329b9ec031196087379
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-04 17:38:26 +0100

    c

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,7 +1,7 @@
-import global.Info;
+import global.*
 
 def call() {
-    Info currentInfo = new Info();
+    def currentInfo = new Info();
     def job_name = currentInfo.job_name;
     echo "${job_name}"
 }
