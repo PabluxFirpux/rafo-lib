b436c840b727a33a81e447cf65f0ec6233557cbb
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-04 18:38:46 +0100

    c

--- vars/buildInfo.groovy
+++ vars/buildInfo.groovy
@@ -1,6 +1,10 @@
 import global.*
 
 def call() {
+    def jenkins = Jenkins.instance;
+    def job = jenkins.getJob("${JOB_NAME}")
+    echo "${job}"
+    echo "===================================="
     Info inf = new Info();
     echo "${JOB_NAME}"
     echo "${WORKSPACE}"
