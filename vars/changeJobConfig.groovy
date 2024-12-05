3704ab0bec560c2de0eaff4a69de27bac49647d3
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 16:35:05 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -1,4 +1,5 @@
 def call() {
     sh "mkdir permisions"
+    sh "touch permisions/out.xml"
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
 }
