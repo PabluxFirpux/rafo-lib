e7a97b71426a2cdc53c6b737827439b0beb54dd4
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 17:30:00 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -3,14 +3,22 @@ import security.*
 def call() {
     sh "mkdir -p ${JENKINS_HOME}/permisions"
     sh "touch  ${JENKINS_HOME}/permisions/out.xml"
-    sh "touch  ${JENKINS_HOME}/permisions/crumb.txt"
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
-    sh "curl -o ${JENKINS_HOME}/permisions/crumb.txt -X GET -u admin:admin http://localhost:8080/crumbIssuer/api/xml"
     def file = new File("${JENKINS_HOME}/permisions/out.xml")
     def fileContent = file.getText()
     def result = PermisionsModifier.addPermission(fileContent, "jenkins_user")
     sh "echo \"${result}\" > ${JENKINS_HOME}/permisions/out.xml"
-    sh "curl -v -X POST --data-binary @${JENKINS_HOME}/permisions/out.xml -u admin:admin -H 'Content-Type: application/xml'  \"http://localhost:8080/job/mec/config.xml\""
+    sh "curl -v -X POST --data-binary @${JENKINS_HOME}/permisions/out.xml -u admin:admin -H 'Content-Type: application/xml'  \"http://localhost:8080/job/mec/config.xml\" -H \".crumb: ${getCrumb()}\""
+}
+
+def getCrumb() {
+    sh "touch  ${JENKINS_HOME}/permisions/crumb.txt"
+    sh "curl -o ${JENKINS_HOME}/permisions/crumb.txt -X GET -u admin:admin http://localhost:8080/crumbIssuer/api/xml"
+    def crumbFile = new File("${JENKINS_HOME}/permisions/crumb.txt")
+    def crumbFileCont = crumbFile.getText()
+    def parts = crumbFileCont.split("<crumb>")
+    def next = parts[1].split("</crumb>")
+    return next[0]
 }
 
 
