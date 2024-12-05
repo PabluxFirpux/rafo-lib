cdfa81440b45f95edffd63de2b0b5b5fd0da746c
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 14:58:03 +0100

    c

--- vars/chowndir.groovy
+++ vars/chowndir.groovy
@@ -1,12 +1,11 @@
 def call(def user, def path) {
     sh "chown -R ${user} ${path}"
 }
-/*
-def chowndir(def user, def path, boolean recursive) {
+
+def call(def user, def path, boolean recursive) {
     if (recursive) {
         chowndir(user, path);
     } else {
         sh "chown ${user} ${path}"
     }
 }
-*/
