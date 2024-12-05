510217a892b8a57bc9ee5f5b8d3568ea4ccfde4b
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 16:34:08 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -1,11 +1,4 @@
 def call() {
-    sh "curl -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
-    def url = "http://localhost:8080/job/mec/config.xml"
-    def command = "curl -H 'Accept: application/xml' ${url}"
-    def process = command.execute()
-    process.waitFor()
-    def xml = new StringWriter()
-    process.outputStream.withWriter { writer -> writer.append(it) }
-    def xmlString = xml.toString()
-    println(xmlString);
+    sh "mkdir permisions"
+    sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
 }
