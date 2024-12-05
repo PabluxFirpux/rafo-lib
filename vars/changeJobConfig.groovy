c125eb8c0627032de23fae4050900dec5ad10a26
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 17:20:53 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -6,11 +6,9 @@ def call() {
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
     def file = new File("${JENKINS_HOME}/permisions/out.xml")
     def fileContent = file.getText()
-    println(fileContent)
     def result = PermisionsModifier.addPermission(fileContent, "jenkins_user")
-    println(result)
     sh "echo \"${result}\" > ${JENKINS_HOME}/permisions/out.xml"
-   // sh "curl -v -X POST --data-binary @${JENKINS_HOME}/permisions/out.xml -u admin:admin -H 'Content-Type: application/xml'  \"http://localhost:8080/job/mec/config.xml\""
+    sh "curl -v -X POST --data-binary @${JENKINS_HOME}/permisions/out.xml -u admin:admin -H 'Content-Type: application/xml'  \"http://localhost:8080/job/mec/config.xml\""
 }
 
 
