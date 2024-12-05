079025ca1e0eb01b19c29126f39cd3ff99fa1011
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 16:43:52 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -4,5 +4,11 @@ def call() {
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
     def file = new File("${JENKINS_HOME}/permisions/out.xml")
     def fileContent = file.getText()
-    println(fileContent)
+    def parts = splitConfig(fileContent)
+    println(parts)
+}
+
+
+def splitConfig(def fileContent) {
+    return fileContent.split("<hudson.security.AuthorizationMatrixProperty>")
 }
