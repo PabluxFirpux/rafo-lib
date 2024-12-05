14d09a23786ff922d321fe0de8baea745bb0c524
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 16:29:28 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -1,3 +1,11 @@
 def call() {
     sh "curl -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
+    def url = "http://localhost:8080/job/mec/config.xml"
+    def command = "curl -H 'Accept: application/xml' ${url}"
+    def process = command.execute()
+    process.waitFor()
+    def xml = new StringWriter()
+    process.outputStream.withWriter { writer -> writer.append(it) }
+    def xmlString = xml.toString()
+    println(xmlString);
 }
