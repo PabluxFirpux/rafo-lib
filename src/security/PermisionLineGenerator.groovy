8569e5cec9aea28b6bf85c5fecc83da486c49366
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-05 17:05:07 +0100

    c

---
+++ src/security/PermisionLineGenerator.groovy
@@ -0,0 +1,11 @@
+package security;
+
+class PermisionLineGenerator {
+    static private def getJobBuild(String username) {
+        return "<permission>USER:hudson.model.Item.Read:${username}</permission>\n"
+    }
+
+    static private def getJobRead(String username) {
+        return "<permission>USER:hudson.model.Item.Read:${username}</permission>\n"
+    }
+}
--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -1,14 +1,13 @@
+import security.*
+
 def call() {
     sh "mkdir -p ${JENKINS_HOME}/permisions"
     sh "touch  ${JENKINS_HOME}/permisions/out.xml"
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
     def file = new File("${JENKINS_HOME}/permisions/out.xml")
     def fileContent = file.getText()
-    def parts = splitConfig(fileContent)
-    println(parts[0])
+    def result = PermisionsModifier.addPermission(fileContent, "jenkins_user")
+    println(result)
 }
 
 
-def splitConfig(def fileContent) {
-    return fileContent.split("<hudson.security.AuthorizationMatrixProperty>")
-}
