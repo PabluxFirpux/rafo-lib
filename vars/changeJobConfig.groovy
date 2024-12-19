3a24e817b3ea18ed451d99d662580f39e20d7ead
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-19 17:01:47 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -4,6 +4,7 @@ def call() {
     sh "mkdir -p ${JENKINS_HOME}/permisions"
     sh "touch  ${JENKINS_HOME}/permisions/out.xml"
     sh "curl -o ${JENKINS_HOME}/permisions/out.xml -X GET -u admin:admin http://localhost:8080/job/mec/config.xml"
+    sh "cat ${JENKINS_HOME}/permisions/out.xml"
     def file = new File("${JENKINS_HOME}/permisions/out.xml")
     def fileContent = file.getText()
     def result = PermisionsModifier.addPermission(fileContent, "jenkins_user")
