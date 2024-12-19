2f1601862ab3620601ec7c0423060434a77e545a
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-19 16:15:24 +0100

    c

--- src/security/PermisionsModifier.groovy
+++ src/security/PermisionsModifier.groovy
@@ -2,7 +2,7 @@ package security;
 
 class PermisionsModifier {
     private static final String sectionHead = "<hudson.security.AuthorizationMatrixProperty>\n";
-    private static final String strat = "<inheritanceStrategy class=\"org.jenkinsci.plugins.matrixauth.inheritance.InheritParentStrategy\"/>\n"
+    private static final String strat = "      <inheritanceStrategy class=\"org.jenkinsci.plugins.matrixauth.inheritance.InheritParentStrategy\"/>\n"
     private static final String sectionTail = "</hudson.security.AuthorizationMatrixProperty>\n";
 
     static def addPermission(String file, String user) {
@@ -10,7 +10,7 @@ class PermisionsModifier {
         def part1 = parts[0]
         parts = parts[1].split(sectionTail)
         def part2 = parts[1]
-        return part1 + sectionHead + strat + PermisionLineGenerator.getJobBuild(user) + PermisionLineGenerator.getJobRead(user) + sectionTail + part2 + "\n";
+        return part1 + sectionHead + strat + PermisionLineGenerator.getJobBuild(user) + PermisionLineGenerator.getJobRead(user) + "    " + sectionTail + part2 + "\n";
     }
 
 
