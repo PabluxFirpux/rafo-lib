e82bc9873e52e0a34ab56a6f9f7b69d5e4c17492
PabluxFirpux <pfirpomolina@gmail.com>
2024-12-19 17:33:58 +0100

    c

--- src/security/PermisionLineGenerator.groovy
+++ src/security/PermisionLineGenerator.groovy
@@ -2,10 +2,10 @@ package security;
 
 class PermisionLineGenerator {
     static private def getJobBuild(String username) {
-        return '      <permission>USER:hudson.model.Item.Build:${username}</permission>\n'
+        return "      <permission>USER:hudson.model.Item.Build:${username}</permission>\n"
     }
 
     static private def getJobRead(String username) {
-        return '      <permission>USER:hudson.model.Item.Read:${username}</permission>\n'
+        return "      <permission>USER:hudson.model.Item.Read:${username}</permission>\n"
     }
 }
