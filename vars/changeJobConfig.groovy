40f013582b1e17c1db1f3b924abff5a2dfed8c7c
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-13 16:22:41 +0100

    c

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -10,7 +10,7 @@ def call(String user, String password, String jobName, String user_to_modify) {
     def file = new File("${full_File_Path}")
     def fileContent = file.getText()
     def result = PermisionsModifier.addPermissions(fileContent, user_to_modify);
-    sh "echo \"${result}\" > ${full_File_Path}"
+    sh "echo ${result} > ${full_File_Path}"
 
     updateConfig(user, password, full_File_Path, download_Path, jobName);
 }
