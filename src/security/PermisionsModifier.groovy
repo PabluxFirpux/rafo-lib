92f650edd170ddd02eca6cb684b3d1f0f2d490e2
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-16 18:51:58 +0100

    advancing

--- src/security/PermisionsModifier.groovy
+++ src/security/PermisionsModifier.groovy
@@ -24,9 +24,28 @@ class PermisionsModifier {
         return xmlString
     }
 
+    static def removePermissions(String text, String user, PermissionTags[] permissions) {
+        def parser = new XmlParser(true, true, true)
+        def project = parser.parseText(text);
+
+        for (PermissionTags permisionTag in permissions) {
+            removePermission(project, user, permisionTag)
+        }
+
+
+        StringWriter stringWriter = new StringWriter()
+        XmlNodePrinter nodePrinter = new XmlNodePrinter(new PrintWriter(stringWriter))
+        nodePrinter.setPreserveWhitespace(true)
+        nodePrinter.setExpandEmptyElements(true)
+        nodePrinter.setQuote("\"")
+
+        nodePrinter.print(project)
+        String xmlString = stringWriter.toString()
+        return xmlString
+    }
+
     static def hasPermission(def permissionNode, String tag) {
         for (def nodes in permissionNode.children()) {
-            println(nodes.value()[0])
             if (nodes.value()[0] == tag) {
                 return true
             }
@@ -34,10 +53,30 @@ class PermisionsModifier {
         return false
     }
 
+    static def getNodeWithPermission(def permissionNode, String tag) {
+        for (def nodes in permissionNode.children()) {
+            if (nodes.value()[0] == tag) {
+                return nodes
+            }
+        }
+        return null
+    }
+
     static def getPermissionNode(def root) {
         def parentNode = root.children().find{ it.name() == 'properties' }
         def permissionNode = parentNode.children().find{it.name() == 'hudson.security.AuthorizationMatrixProperty'}
         return permissionNode;
+    }
+
+    static def removePermission(def root, def user, PermissionTags permission) {
+        def permissionNode = getPermissionNode(root)
+        def permissionString = PermisionLineGenerator.getPermissionStringByEnum(permission)
+        def fullString = "${permissionString}:${user}";
+
+        if (hasPermission(permissionNode, fullString)) {
+            def node = getNodeWithPermission(permissionNode, fullString)
+            permissionNode.remove(node)
+        }
     }
 
     static def addPermission(def root, def user, PermissionTags permission) {
---
+++ vars/removeJobPermissions.groovy
@@ -0,0 +1,9 @@
+import security.*
+
+def call(String user, String password, String jobName, String user_to_modify, PermissionTags[] tags) {
+
+    def fileContent = getJobConfig(user, password, jobName)
+    def result = PermisionsModifier.removePermissions(fileContent, user_to_modify, tags);
+
+    updateJobConfig(user, password, jobName, result)
+}
