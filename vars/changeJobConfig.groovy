a14182030242982687cc2f328e032c76153f7f9b
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-16 18:17:59 +0100

    Classpath correct

--- vars/changeJobConfig.groovy
+++ vars/changeJobConfig.groovy
@@ -1,10 +1,10 @@
 import security.*
 
 def call(String user, String password, String jobName, String user_to_modify, PermissionTags[] tags) {
-    /*def download_Path = "${JENKINS_HOME}/permisions";
+    def download_Path = "${JENKINS_HOME}/permisions";
     def file_Name = "output.xml"
     def full_File_Path = "${download_Path}/${file_Name}"
-
+/*
     downloadFile(user, password, jobName, download_Path, full_File_Path);
 
     def file = new File("${full_File_Path}")
