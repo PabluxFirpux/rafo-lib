53b723e14f15b85c04ef79a78e51a76d7b757915
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-17 00:13:37 +0100

    Done

--- vars/deleteAllPermissions.groovy
+++ vars/deleteAllPermissions.groovy
@@ -3,6 +3,5 @@ import security.PermisionsModifier
 def call(String user, String password, String jobName, String userToRemove) {
     def fileContent = getJobConfig(user, password, jobName)
     def result = PermisionsModifier.deleteAllUserPermissions(fileContent, userToRemove)
-    println(result)
     updateJobConfig(user, password, jobName, result)
 }
