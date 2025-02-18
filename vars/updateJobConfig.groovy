9df3bdff7cd34ba506a97f353c2a111b82c291e5
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 16:55:13 +0100

    Done

--- vars/updateJobConfig.groovy
+++ vars/updateJobConfig.groovy
@@ -7,11 +7,5 @@ def call(String jobName, String newFileText) {
 
     File newFile = new File("${full_File_Path}")
     newFile.write("${newFileText}")
-    updateConfig(user, password, full_File_Path, download_Path, jobName)
-}
-
-def updateConfig(String user, String password, String fullPath, String downloadPath, String jobName) {
-    def crumb = getCrumb(user, password, downloadPath);
-    sh "curl -v -X POST --data-binary @${fullPath} -u ${user}:${password} -H 'Content-Type: application/xml'  \"${JENKINS_URL}job/${jobName}/config.xml\" -H 'Jenkins-Crumb: ${crumb}'"
-
+    postConfig(user, password, full_File_Path, download_Path, jobName)
 }
