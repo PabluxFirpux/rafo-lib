d68397ba7fd416ee4f1ea505c8983ddf5c437e2f
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 14:59:22 +0100

    Done

--- vars/addJobPermissions.groovy
+++ vars/addJobPermissions.groovy
@@ -3,7 +3,7 @@ import security.*
 def call(String jobName, String user_to_modify, PermissionTags[] tags) {
     def user = getUser()
     def password = getPassword()
-    def fileContent = getJobConfig(user, password, jobName)
+    def fileContent = getJobConfig(jobName)
     def result = PermisionsModifier.addPermissions(fileContent, user_to_modify, tags);
 
     updateJobConfig(jobName, result)
