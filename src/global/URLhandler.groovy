7d80e2637e3bdfeae6855b2d7100ee5c26a5c855
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 16:45:05 +0100

    Done

--- src/global/URLhandler.groovy
+++ src/global/URLhandler.groovy
@@ -6,10 +6,10 @@ class URLhandler {
         def result = ""
         def path = jobName.split("/")
         println(path.size())
-        for (int i = 0; i < path.length-1; i++) {
+        for (int i = 0; i < path.size-1; i++) {
             result += "job/${path[i]}/"
         }
-        result += "createItem?name=${path[path.length]}"
+        result += "createItem?name=${path[path.size-1]}"
         println(result)
         return result;
     }
