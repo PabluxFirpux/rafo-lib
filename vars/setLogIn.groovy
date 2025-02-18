4ec90d51034fb2a7d4b7b2e985bf7110efad0054
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 14:21:05 +0100

    Done

--- vars/setLogIn.groovy
+++ vars/setLogIn.groovy
@@ -2,6 +2,6 @@ import security.LogIn
 
 def call(String user, String password) {
     //LogIn.setLogIn(user, password)
-    sh "env.JENKINS_USER = ${user}"
-    sh "env.JENKINS_PASS = ${password}"
+//    sh "env.JENKINS_USER = ${user}"
+  //  sh "env.JENKINS_PASS = ${password}"
 }
