3c8eb7a86f97da258a4fc1eec48279bd646f8bd8
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 14:12:03 +0100

    Done

--- vars/setLogIn.groovy
+++ vars/setLogIn.groovy
@@ -2,6 +2,6 @@ import security.LogIn
 
 def call(String user, String password) {
     //LogIn.setLogIn(user, password)
-    sh "export USERNAME=${user}"
-    sh "export JENKINS_API_KEY=${password}"
+    sh "export USERNAME=\"${user}\""
+    sh "export JENKINS_API_KEY=\"${password}\""
 }
