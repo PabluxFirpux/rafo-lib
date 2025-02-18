b3afbfd6f79af06aa6f35afe09eeba9b5d3d74e9
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 16:49:00 +0100

    Done

--- src/global/URLhandler.groovy
+++ src/global/URLhandler.groovy
@@ -2,10 +2,8 @@ package global
 
 class URLhandler {
     public static def getCreateJobString(String jobName) {
-        println(jobName)
         def result = ""
         def path = jobName.split("/")
-        println(path.size())
         for (int i = 0; i < path.size()-1; i++) {
             result += "job/${path[i]}/"
         }
--- vars/createNewJob.groovy
+++ vars/createNewJob.groovy
@@ -14,12 +14,3 @@ def call(String jobName) {
     newFile.write("${newJobText}")
     postConfig(user, password, full_File_Path, download_Path, jobName)
 }
-
-def postConfig(String user, String password, String fullPath, String downloadPath, String jobName) {
-    def crumb = getCrumb(user, password, downloadPath);
-    println("goten crumb")
-    def correctPath = URLhandler.getCreateJobString(jobName)
-    def url = "${JENKINS_URL}${correctPath}"
-    sh "curl -v -X POST --data-binary @${fullPath} -u ${user}:${password} -H 'Content-Type: application/xml'  \"${url}\" -H 'Jenkins-Crumb: ${crumb}'"
-
-}
