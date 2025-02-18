9d75316e315b0a7462094f548633fa85f1888e79
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-18 17:19:01 +0100

    Done

--- vars/deleteJob.groovy
+++ vars/deleteJob.groovy
@@ -6,7 +6,7 @@ def call(String jobName) {
     def downloadPath = getDownloadPath();
     sh "rm -rf ${downloadPath}/*"
     def crumb = getCrumb(user, password, downloadPath);
-    def correctPath = URLhandler.getCreateJobString(jobName)
+    def correctPath = URLhandler.getRegularJobString(jobName)
     def url = "${JENKINS_URL}${correctPath}/doDelete"
     sh "curl -X POST -u ${user}:${password}  \"${url}\" -H 'Jenkins-Crumb: ${crumb}'"
 }
