67984d0747e95446fdfbc6766cf56b420912a555
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-19 14:27:58 +0100

    Done

--- vars/jobIsFolder.groovy
+++ vars/jobIsFolder.groovy
@@ -1,11 +1,8 @@
 import groovy.xml.*
 
 def call(String jobName) {
-    if (!jobExists(jobName)) return false
-
-    def response = getJobConfig(jobName)
-    def parser = new XmlParser(true, true, true)
-    def responseXML = parser.parseText(response);
-    //def targetNode = responseXML.children().find{ it.name() == 'properties' }
-    println(responseXML.name())
+    def type = jobGetType(jobName)
+    def expected = "com.cloudbees.hudson.plugins.folder.Folder"
+    if (type == expected) return true
+    else return false
 }
