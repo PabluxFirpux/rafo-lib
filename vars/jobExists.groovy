15daece108658ed97bf6489187c9e4f4dbf7f214
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-19 14:16:31 +0100

    Done

--- vars/jobExists.groovy
+++ vars/jobExists.groovy
@@ -4,9 +4,11 @@ import groovy.xml.*
 def call(String jobName) {
     def response = getJobConfig(jobName)
     def parser = new XmlParser(true, true, true)
-    def responseXML = parser.parseText(response);
-    //def titleNode = responseXML.children().find{ it.name() == 'title' }
-
-    //def result = XMLmanipulation.nodeToString(titleNode)
-    println(responseXML)
+    def responseXML;
+    try {
+        responseXML = parser.parseText(response);
+        println(true)
+    } catch (Exception e) {
+        println(false)
+    }
 }
