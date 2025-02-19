0975499535fda8677ea587c6f8871ef47de761b1
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-19 14:28:53 +0100

    Done

--- vars/jobIsFolder.groovy
+++ vars/jobIsFolder.groovy
@@ -3,6 +3,12 @@ import groovy.xml.*
 def call(String jobName) {
     def type = jobGetType(jobName)
     def expected = "com.cloudbees.hudson.plugins.folder.Folder"
-    if (type == expected) return true
-    else return false
+    if (type == expected) {
+        println(true)
+        return true
+    }
+    else {
+        println(false)
+        return false
+    }
 }
