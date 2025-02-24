caa08689e747954ae66e46bb620b111c41bac35f
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-24 15:19:35 +0100

    Done

--- vars/giveAllPermissions.groovy
+++ vars/giveAllPermissions.groovy
@@ -2,5 +2,7 @@ import security.PermissionTags
 
 def call(String jobName, String user) {
     PermissionTags[] allTags = PermissionTags.values()
-    println(allTags)
+    for (PermissionTags tag in allTags) {
+        println(tag)
+    }
 }
