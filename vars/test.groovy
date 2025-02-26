fd3da78fb6f6d6677546667574c5492dac3ca000
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-26 17:15:02 +0100

    Done

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,8 @@
 @GrabResolver(name='xbib-releases', root='https://repo1.maven.org/maven2/')
 @Grab(group='org.xbib.groovy', module='groovy-ldap', version='1.0.2')
-import org.xbib.groovy.groovyldap.*;
+import org.xbib.groovy.ldap.LDAP
+import org.xbib.groovy.ldap.SearchScope
+
 
 def call() {
     def ldapUrl = 'ldap://ldap.mycompany.com:389/dc=mycompany,dc=com'
