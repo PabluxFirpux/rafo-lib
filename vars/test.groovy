582b12e9b4f1181b1b9c3cd050f2f98c08a4ca35
pabluxfirpux <pfirpomolina@gmail.com>
2025-02-26 17:25:22 +0100

    Done

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,9 +11,20 @@ def call() {
 
     def ldap = LDAP.newInstance(ldapUrl, bindDN, bindPassword)
 
-    ldap.eachEntry('dc=example,dc=com', '(objectClass=person)', SearchScope.SUB) { entry ->
-        println "Found entry: ${entry.dn}"
-    }
+    def username = "euler"
+    def userEntry = ldap.searchUnique("uid=${username},ou=mathematicians,dc=example,dc=com")
 
+    if (userEntry) {
+        try {
+            // Attempt to bind using the user's DN and password
+            def userLdap = LDAP.newInstance(ldapUrl, userEntry.dn, "password")
+            println "Authentication successful for user: ${username}"
+            userLdap.close()
+        } catch (Exception e) {
+            println "Authentication failed for user: ${username} - ${e.message}"
+        }
+    } else {
+        println "User not found: ${username}"
+    }
     ldap.close()
 }
